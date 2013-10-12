76e9e4615f63c1166f7702189586a3f44f76d8fc
iMasonite <mason1985@live.co.uk>
2013-10-12 18:11:01 +0100

    deleted

--- ignore/configuration/Configuration.java
+++
@@ -1,6 +0,0 @@
-
-package com.thorpscraft.imasonite.configuration;
-/** General configuration operations and methods and file handling.
- * 
- * @author Michael Mason */
-public abstract class Configuration {}
